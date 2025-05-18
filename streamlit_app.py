12e9e1fb3bf375f08ef34cdcecaf3e49a06d8e2b
Emiliano-Tonatiuh <65556190+Emiliano-Tonatiuh@users.noreply.github.com>
2025-05-17 23:05:02 -0600

    Update streamlit_app.py
    inclunding nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw: {st.__version__}")
 st.write(
@@ -31,6 +31,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+      
 
     #st.write(ingredients_string)
 
@@ -42,3 +43,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered, {name_on_order}",  icon="✅")
+      
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
